af58967cdac26a7bda8d018cd60247023154d75c
Dhanavarsha <dhan.varsha03@gmail.com>
2016-07-10 15:08:41 +0530

    Code changed. Null Pointer Exception present

--- src/com/tests/MortgageCalcsTest.java
+++ src/com/tests/MortgageCalcsTest.java
@@ -1,7 +1,6 @@
 package com.tests;
 
 import com.appModules.CalculateMortgagePayments;
-import com.pageObjects.MortgagePaymentCalculator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
@@ -27,7 +26,7 @@ public class MortgageCalcsTest {
     }
 
     @Test
-    public void mortagePaymentCalculator30YearFixed(WebDriver driver) {
+    public void mortagePaymentCalculator30YearFixed() {
         System.out.println("Before Test");
         CalculateMortgagePayments.calculatePayment(driver,300000.00, 4.5, 400000, 2500, 1500, 0.55);
         System.out.println("After Test");
