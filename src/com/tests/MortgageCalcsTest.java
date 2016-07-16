396fc0a4f989e361887a57cc7238946b44640a45
Dhanavarsha <dhan.varsha03@gmail.com>
2016-07-17 00:27:13 +0530

    Assertions Added

--- src/com/tests/MortgageCalcsTest.java
+++ src/com/tests/MortgageCalcsTest.java
@@ -29,16 +29,13 @@ public class MortgageCalcsTest {
 
         MortgagePaymentCalculatorPage mortgagePaymentCalculatorPage = new MortgagePaymentCalculatorPage(driver);
         mortgagePaymentCalculatorPage.calculate(300000.00, 4.5, 400000, 2500, 1500, 0.55);
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         System.out.println("Before Assert");
         Assert.assertEquals("Financial Analysis", driver.findElement(By.xpath("//*[@id=\"analysisDiv\"]/div/font/strong")).getText());
         Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"analysisDiv\"]/table/tbody/tr[1]/td[2]")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"analysisDiv\"]/table/tbody/tr[2]/td[2]")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"analysisDiv\"]/table/tbody/tr[3]/td[2]")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"analysisDiv\"]/table/tbody/tr[4]/td[2]")).isDisplayed());
-        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"analysisDiv\"]/table/tbody/tr[5]/td[2]")).isDisplayed());
-        Assert.assertTrue(driver.findElement(By.cssSelector("#analysisDiv > table > tbody > tr:nth-child(6) > td.last")).isDisplayed());
-        Assert.assertTrue(driver.findElement(By.cssSelector("#analysisDiv > table > tbody > tr:nth-child(7) > td.last")).isDisplayed());
         System.out.println("After assert");
     }
 
