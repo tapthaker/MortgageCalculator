6a4f6dc5b4a7afa71ecc96e805a1ab76fe0c91d1
Dhanavarsha <dhan.varsha03@gmail.com>
2016-07-16 21:38:42 +0530

    Added existing code to a new Class CalculateMortgagePayment.java

--- src/com/pageObjects/MortgagePaymentCalculatorPage.java
+++ src/com/pageObjects/MortgagePaymentCalculatorPage.java
@@ -76,7 +76,7 @@ public class MortgagePaymentCalculatorPage {
         calculateButton.click();
         driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
         try {
-            Thread.sleep(2000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
